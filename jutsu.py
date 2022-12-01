dea9632f446c729d762727ef2d9589a8d1302611
cryptosvinarnik <na4030680@gmail.com>
2022-12-01 07:13:49 +0200

    paranoik govnokodik fixes and disabled async

--- jutsu.py
+++ jutsu.py
@@ -1,4 +1,3 @@
-import asyncio
 import os
 from typing import List
 
@@ -6,10 +5,10 @@ import aiofiles
 import aiohttp
 from bs4 import BeautifulSoup
 
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0"}
+from config import DIR, HEADERS, LINK
 
 
-class Episode():
+class Episode:
     def __init__(self, episode_name: str, href: str) -> None:
         self.name = episode_name
         self.href = href
@@ -17,22 +16,16 @@ class Episode():
         self.season = href.split("/")[2] if "season" in href else "season-1"
 
 
-class JutSu():
-
-    LINK = "https://jut.su"
-
+class JutSu:
     def __init__(self, slug: str) -> None:
         self.slug = slug
-        self.client = aiohttp.ClientSession(headers=headers)
+        self.client = aiohttp.ClientSession(headers=HEADERS)
 
-    async def __aenter__(self):
-        return self
-
-    async def __aexit__(self, *args, **kwargs) -> None:
+    async def close(self) -> None:
         await self.client.close()
 
     async def get_all_episodes(self) -> List[Episode]:
-        main_page = await self.client.get(f"{self.LINK}/{self.slug}")
+        main_page = await self.client.get(f"{LINK}/{self.slug}")
 
         soup = BeautifulSoup(await main_page.text(), "html.parser")
 
@@ -41,11 +34,21 @@ class JutSu():
         return [Episode(episode.text, episode.attrs["href"]) for episode in episodes]
 
     async def get_download_link(self, href: str, res: str) -> str:
-        episode_page = await self.client.get(f"{self.LINK}/{href}")
+        episode_page = await self.client.get(f"{LINK}/{href}")
 
         soup = BeautifulSoup(await episode_page.text(), "html.parser")
 
-        return soup.find("source", {"res": res}).attrs["src"]
+        source = soup.find("source", {"res": res})
+        source = source if source else soup.find("source")
+
+        return source.attrs["src"] if source else None
+
+
+async def get_link_and_download(inst: JutSu, episode: Episode, res: str):
+    link = await inst.get_download_link(episode.href, res)
+
+    if link:
+        await download_video(link, f"{DIR}/{inst.slug}/{episode.season}/{episode.name}.mp4")
 
 
 async def download_video(link: str, path: str) -> None:
@@ -54,38 +57,10 @@ async def download_video(link: str, path: str) -> None:
 
     print(f"Start downloading {path}")
 
-    async with aiohttp.ClientSession(raise_for_status=True, headers=headers) as cli:
+    async with aiohttp.ClientSession(raise_for_status=True, headers=HEADERS) as cli:
         async with cli.get(link) as r:
             async with aiofiles.open(path, "wb+") as f:
                 async for d in r.content.iter_any():
                     await f.write(d) if d else None
 
     print(f"{path} downloaded!")
-
-
-async def main():
-    slug = input("Enter the link: ").split("/")[3]
-    res = input("Enter the resolution (1080, 720, 480, 360): ")
-
-    download_type = input("Download synchronously or asynchronously? (1, 2): ")
-
-    if res not in ("1080", "720", "480", "360"):
-        return print("Enter the correct resolution!")
-
-    async with JutSu(slug) as jutsu:
-        episodes = await jutsu.get_all_episodes()
-
-        links = [await jutsu.get_download_link(episode.href, res) for episode in episodes]
-
-    if download_type == "1":
-        for episode, link in zip(episodes, links):
-            await download_video(link, f"{slug}/{episode.season}/{episode.name}.mp4")
-
-    elif download_type == "2":
-        tasks = [
-            asyncio.create_task(
-                download_video(link, f"{slug}/{episode.season}/{episode.name}.mp4")
-            ) for episode, link in zip(episodes, links)
-        ]
-
-        await asyncio.gather(*tasks)
