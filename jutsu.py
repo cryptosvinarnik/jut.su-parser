45147229a80e83656a94d688c0f6c971e1e94f2a
amanzholov <freelance1@gmail.com>
2024-04-18 11:52:30 +0500

    Patched bug with connecting to server (Just turned off SSL, and it worked).
    Added support to download by season (Just enter the link with season),
    
    Async mode is added
    
    Showing the download percentage (only on sync mode)

--- jutsu.py
+++ jutsu.py
@@ -3,6 +3,7 @@ from typing import List
 
 import aiofiles
 import aiohttp
+from aiohttp import TCPConnector
 from bs4 import BeautifulSoup
 
 from config import DIR, HEADERS, LINK
@@ -19,13 +20,20 @@ class Episode:
 class JutSu:
     def __init__(self, slug: str) -> None:
         self.slug = slug
-        self.client = aiohttp.ClientSession(headers=HEADERS)
+        self.connector = TCPConnector(ssl=False)
+        self.client = aiohttp.ClientSession(headers=HEADERS, connector=self.connector)
 
     async def close(self) -> None:
         await self.client.close()
 
-    async def get_all_episodes(self) -> List[Episode]:
-        main_page = await self.client.get(f"{LINK}/{self.slug}")
+    async def get_all_episodes(self, season: str = None) -> List[Episode]:
+        url = f"{LINK}/{self.slug}"
+        if season:
+            url += f"/{season}"
+
+        print('url:', url)
+
+        main_page = await self.client.get(url)
 
         soup = BeautifulSoup(await main_page.text(), "html.parser")
 
@@ -44,23 +52,31 @@ class JutSu:
         return source.attrs["src"] if source else None
 
 
-async def get_link_and_download(inst: JutSu, episode: Episode, res: str):
+async def get_link_and_download(inst: JutSu, episode: Episode, res: str, show_percentage: bool = True) -> None:
+    cli = inst.client
     link = await inst.get_download_link(episode.href, res)
 
     if link:
-        await download_video(link, f"{DIR}/{inst.slug}/{episode.season}/{episode.name}.mp4")
+        await download_video(cli, link, f"{DIR}/{inst.slug}/{episode.season}/{episode.name}.mp4",
+                             show_percentage=show_percentage)
 
 
-async def download_video(link: str, path: str) -> None:
+async def download_video(cli: aiohttp.ClientSession, link: str, path: str, show_percentage: bool = True) -> None:
     if not os.path.exists(path):
         os.makedirs("/".join(path.split("/")[:-1]), exist_ok=True)
 
     print(f"Start downloading {path}")
 
-    async with aiohttp.ClientSession(raise_for_status=True, headers=HEADERS) as cli:
-        async with cli.get(link) as r:
-            async with aiofiles.open(path, "wb+") as f:
-                async for d in r.content.iter_any():
-                    await f.write(d) if d else None
+    # print percentage of download
+    async with cli.get(link) as r:
+        total_size = int(r.headers.get('Content-Length', 0))
+        downloaded_size = 0
+        async with aiofiles.open(path, "wb+") as f:
+            async for d in r.content.iter_any():
+                downloaded_size += len(d)
+                percentage = (downloaded_size / total_size) * 100
+                if show_percentage:
+                    print(f"\rDownloaded {percentage:.2f}%", end="")
+                await f.write(d) if d else None
 
     print(f"{path} downloaded!")
